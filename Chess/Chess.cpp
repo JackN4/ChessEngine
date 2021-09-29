94c9bbb015f9dfbc4957c41acbb1cc877b7cf799
jsnewton <jsnewton4@gmail.com>
2021-09-29 18:49:09 +0100

    Fixes conflicts

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -115,11 +115,11 @@ int main()
             int score = evaluator.eval(board);
             cout << "eval: " << score << "\n";
         }
-        else if (input.rfind("go", 0) == 0) { //Finds best move
+        else if (input.rfind("go", 0) == 0) {
             Search search;
-            Move bestMove = search.negamax_iter(board); //Searchs for best  move
-            cout << "bestmove " << bestMove.move_to_lerf() << "\n"; //Outputs best move
-        } 
+            Move bestMove = search.negamax_start(board);
+            cout << "bestmove " << bestMove.move_to_lerf() << "\n";
+        }
         else if (input == "zobrist") { //Outputs current zobrist key
             cout << (board.zobristKey) << "\n";
         }
