e16df147bf5ad49d519740879b6c4e8fbe520d88
jsnewton <jsnewton4@gmail.com>
2021-09-29 18:38:33 +0100

    Added comments

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -17,39 +17,39 @@ using namespace std;
 string engineName = "Jack's engine";
 string author = "Jack";
 
+//This is the file where the program starts and interacts with command line
 
 int main()
 {
-    Lookups lookups;
+    Lookups lookups; //Creates lookup tables
     Board board; //Create Board
-    BoardDisplay display;
-    string input;
-    bool debug = false;
-    while (true) {
-        getline(cin, input);
-        if (input == "uci") {
+    BoardDisplay display; //Creates object to display board
+    string input; //Creates string to get input
+    bool debug = false; //changes debug mode to show more move
+    while (true) { //Runs until program ends
+        getline(cin, input); //Get user input
+        if (input == "uci") { //Prints engine info
             cout << "id name" << engineName << "\n";
             cout << "id author" << author << "\n";
             cout << "uciok" << "\n";
         }
-        else if (input == "ucinewgame") {
+        else if (input == "ucinewgame") { //Sets up for new game
             cout << "newGame" << "\n";
-            //engine setup message
         }
-        else if (input.rfind("setoption", 0) == 0) {
+        else if (input.rfind("setoption", 0) == 0) { //Sets options
             cout << "setOptions" << "\n";
             //setup for new game
         }
-        else if (input == "isready") {
+        else if (input == "isready") { //Says its ready for new command
             cout << "readyok" << "\n";
         }
-        else if (input.rfind("position", 0) == 0) { //changed so needs fen
+        else if (input.rfind("position", 0) == 0) { //Takes in new board position
             string FEN;
             int movesPos = input.find("moves");
-            if (input.find("startpos") != string::npos) {
+            if (input.find("startpos") != string::npos) { //Sets the FEN string to starting position
                 FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
             }
-            else if (input.find("fen") != string::npos) {
+            else if (input.find("fen") != string::npos) { //Gets FEN string if its not start position
                 if (movesPos == string::npos) {
                     FEN = input.substr(13);
                 }
@@ -58,12 +58,12 @@ int main()
                 }
             }
             board.create_from_FEN(FEN);
-            if (movesPos != string::npos) {
+            if (movesPos != string::npos) { //Finds the strings for the moves which have been played and makes the moves on the board
                 std::istringstream iss(input.substr(movesPos + 5));
                 string move;
                 while (std::getline(iss, move, ' ')) {
                     if (move.length() == 4 || move.length() == 5) {
-                        board.make_move_notation(move);
+                        board.make_move_notation(move); //Makes move on board using move string
                     }
                 }
             }
@@ -71,41 +71,36 @@ int main()
                 display.display_board(board);
             }
         }
-        else if (input == "moves debug") {
+        else if (input == "moves debug") { //Gets list of current moves for debugging purposes
             MoveCreator moveGen = MoveCreator(board);
             vector<Move> moves = moveGen.get_all_moves();
             display.display_all_moves(moves);
         }
-        else if (input == "display") {
+        else if (input == "display") { //Displays boards
             display.display_board(board);
         }
-        else if (input.rfind("perftNB", 0) == 0) {
+        else if (input.rfind("perftNB", 0) == 0) { //Performs perft without batch counting
             Perft perft;
-            string depthStr = input.substr(8);
+            string depthStr = input.substr(8); //Gets depth to seach to
             depthStr.erase(remove_if(depthStr.begin(), depthStr.end(), isspace), depthStr.end());
             int depth = stoi(depthStr);
             perft.calculate_perft(board, depth, debug);
         }
-        else if (input.rfind("perftTT", 0) == 0) {
+        else if (input.rfind("perftTT", 0) == 0) { //Performs perft using the transposition table
             Perft perft;
-            string depthStr = input.substr(8);
+            string depthStr = input.substr(8);//Gets depth to seach to
             depthStr.erase(remove_if(depthStr.begin(), depthStr.end(), isspace), depthStr.end());
             int depth = stoi(depthStr);
             perft.calculate_perft_TT(board, depth, debug);
         }
-        /*else if (input.rfind("perftFile", 0) == 0) {
-            Perft perft;
-            string fileName = input.substr(10);
-            perft.perft_file(fileName);
-        }*/
-        else if (input.rfind("perft", 0) == 0) {
+        else if (input.rfind("perft", 0) == 0) { //Performs perft with batch testing
             Perft perft;
             string depthStr = input.substr(6);
             depthStr.erase(remove_if(depthStr.begin(), depthStr.end(), isspace), depthStr.end());
             int depth = stoi(depthStr);
             perft.calculate_perft_bulk(board, depth, debug);
         }
-        else if (input == "debug") {
+        else if (input == "debug") { //Toggles debug mode
             if (debug) {
                 cout << "debug mode is now off\n";
                 debug = false;
@@ -115,17 +110,17 @@ int main()
                 debug = true;
             }
         }
-        else if (input == "eval") {
+        else if (input == "eval") { //Prints current static evaluation of board
             Evaluator evaluator;
             int score = evaluator.eval(board);
             cout << "eval: " << score << "\n";
         }
-        else if (input.rfind("go", 0) == 0) {
+        else if (input.rfind("go", 0) == 0) { //Finds best move
             Search search;
-            Move bestMove = search.negamax_iter(board);
-            cout << "bestmove " << bestMove.move_to_lerf() << "\n";
-        }
-        else if (input == "zobrist") {
+            Move bestMove = search.negamax_iter(board); //Searchs for best  move
+            cout << "bestmove " << bestMove.move_to_lerf() << "\n"; //Outputs best move
+        } 
+        else if (input == "zobrist") { //Outputs current zobrist key
             cout << (board.zobristKey) << "\n";
         }
         else {
