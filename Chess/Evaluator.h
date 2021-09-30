b5bf397ac93fec95f5d1ac2c00f71b1cf3ddecc5
jsnewton <jsnewton4@gmail.com>
2021-09-30 01:10:15 +0100

    Added comments

--- Chess/Evaluator.h
+++ Chess/Evaluator.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "Board.h"
 #include "BitOperations.h"
+
+//This file evaluates the current position on the board
+
 class Evaluator
 {
 	BitOperations bitOp;
-public:int vals[5] = { 100, 320, 330, 500, 900 };
-	int PSTs[2][4][64] = { {
+public:int vals[5] = { 100, 320, 330, 500, 900 }; //Piece values
+	int PSTs[2][4][64] = { { //These tables give scores dependent on where a specific piece of a specific colour is on the board
 		{0,  0,  0,  0,  0,  0,  0,  0,
 	50, 50, 50, 50, 50, 50, 50, 50,
 	10, 10, 20, 30, 30, 20, 10, 10,
@@ -78,14 +81,14 @@ public:int vals[5] = { 100, 320, 330, 500, 900 };
 	};
 
 
-public: int eval(Board& board) {
+public: int eval(Board& board) { //Function is given a board and returns an evaluation
 	int score = 0;
 	for (int i = 0; i < 5; i++) {
-		score += get_pop_count(board, 0, i + 2) * vals[i];
-		score -= get_pop_count(board, 1, i + 2) * vals[i];
+		score += get_pop_count(board, 0, i + 2) * vals[i]; //Adds values of all white pieces
+		score -= get_pop_count(board, 1, i + 2) * vals[i]; //Subtracts value of all black pieces
 	}
-	score += get_pst_val(board)/2; 
-	if (board.toMove == white) {
+	score += get_pst_val(board)/2; //Gets score based on piece positions
+	if (board.toMove == white) { //Returns score relative to current player
 		return score;
 	}
 	else {
@@ -93,11 +96,11 @@ public: int eval(Board& board) {
 	}
 }
 
-private: int get_pop_count(Board& board, int colour, int piece) {
+private: int get_pop_count(Board& board, int colour, int piece) { //Gets number of specific piece of specific colour on the board
 	return bitOp.pop_count(board.bitboards[colour] & board.bitboards[piece]);
 }
 
-private: int get_pst_val(Board& board) {
+private: int get_pst_val(Board& board) { //Sums all the position based scores
 	int score = 0;
 	score += get_pst_BB(board, white, pawn,0);
 	score -= get_pst_BB(board, black, pawn,0);
@@ -112,16 +115,14 @@ private: int get_pst_val(Board& board) {
 
 private: int get_pst_BB(Board& board, int colour, Piece piece, int pstNum) {
 	int score = 0;
-	pair<int*, int> fullScan = bitOp.full_bitscan(board.get_piece_BB(colour, piece));
+	pair<int*, int> fullScan = bitOp.full_bitscan(board.get_piece_BB(colour, piece)); //Gets the positions of all the piece
 	int pos;
-	for (int i = 0; i < fullScan.second; i++) {
+	for (int i = 0; i < fullScan.second; i++) { //iterates through all the pieces
 		pos = fullScan.first[i];
-		score += PSTs[colour][pstNum][pos];
+		score += PSTs[colour][pstNum][pos]; //Adds their value to the score
 	}
 	return score;
 }
-/*private: int get_pst_BB(Board& board, int colour, Piece piece) {
-	return get_pst_BB(board, colour, piece, piece);
-}*/
+
 };
 
--- Chess/Move.h
+++ Chess/Move.h
@@ -7,15 +7,21 @@ using namespace N;
 using std::list;
 using std::string;
 
+//This defines the structure Move which is used to store all needed info about a move
+
 
 struct Move
 {
-int startPos, endPos, castling, castlingBefore[2], enPassantBefore;
-Piece pieceType, pieceCapture, promoPiece;
-//public: list<Pinned> addPinned[2], removedPinned[2];
+//Position before move, position after move, if move is a castle, the castling rights before move, en passant rights before move
+int startPos, endPos, castling, castlingBefore[2], enPassantBefore; 
+
+//Type of piece in move, piece that was captured, if there is a promotion what piece to promote to
+Piece pieceType, pieceCapture, promoPiece; 
+
+//If move is en passant
 bool enPassant;
 
-public: Move(Piece pieceIn, int startPosin, int endPosIn, Piece pieceCIn=white, int castlingIn = 0, Piece promoPieceIn = white, int enPassantIn = 0) {
+public: Move(Piece pieceIn, int startPosin, int endPosIn, Piece pieceCIn=white, int castlingIn = 0, Piece promoPieceIn = white, int enPassantIn = 0) { //Sets all variables to given arguments
 	pieceType = pieceIn;
 	pieceCapture = (Piece)pieceCIn;
 	castling = castlingIn; //kq does it castle
@@ -25,18 +31,18 @@ public: Move(Piece pieceIn, int startPosin, int endPosIn, Piece pieceCIn=white, 
 	enPassant = enPassantIn;//the piece that is remove
 }
 
-public: Move() {
+public: Move() { //Creates empty move
 	startPos = 0;
 	endPos = 0;
 }
 
 
 
-public: string move_to_lerf() {
+public: string move_to_lerf() { //Turns move into standard notation
 	string result;
 	result += sqr_to_lerf(startPos);
 	result += sqr_to_lerf(endPos);
-	switch (promoPiece) { //don't like this way but it works and doesnt need to be fast
+	switch (promoPiece) { 
 	case white:
 		break;
 	case knight:
@@ -55,7 +61,7 @@ public: string move_to_lerf() {
 	return result;
 }
 
-private: string sqr_to_lerf(int sqr) {
+private: string sqr_to_lerf(int sqr) { //Gets the rank and file of a square at turns it into standard notation
 	char rank = (sqr >> 3) + '1';
 	char file = (sqr & 7) + 'a';
 	string result;
