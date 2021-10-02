5c0adc0eeba35ad1889a3cb4f919d1ea2acb4ccf
jsnewton <jsnewton4@gmail.com>
2021-10-02 16:42:25 +0100

    Added comments

--- Chess/PerftTable.h
+++ Chess/PerftTable.h
@@ -6,20 +6,22 @@
 using std::pair;
 using std::make_pair;
 
+//This file creates a transposition table to be used by perftt to increase perft speed and debug zobrist keys
+
 struct EntryPerft{
-		uint64_t key;
-		uint64_t count;
-		int depth;
-	public: EntryPerft(uint64_t keyIn, uint64_t countIn, int depthIn) {
+		uint64_t key; //Zobrist key
+		uint64_t count; //Number of moves
+		int depth; //Depth board was searched to
+	public: EntryPerft(uint64_t keyIn, uint64_t countIn, int depthIn) { //Create new entry
 		key = keyIn;
 		count = countIn;
 		depth = depthIn;
 	}
-public: EntryPerft() {
-	key = 0;
-	count = 0;
-	depth = 0;
-	}
+	public: EntryPerft() { //Creates empty entry
+		key = 0;
+		count = 0;
+		depth = 0;
+		}
 };
 
 
@@ -27,22 +29,20 @@ public: EntryPerft() {
 
 class PerftTable
 {	
-	int tableSize = 0x2FFFFF;
-	EntryPerft* table = new EntryPerft[tableSize];
-	public: void add(EntryPerft entry) { //changes index to new entry so always replaces
-		table[entry.key % tableSize] = entry;
+	int tableSize = 0x2FFFFF; //The number of entries one table can have
+	EntryPerft* table = new EntryPerft[tableSize]; //Creates array
+	public: void add(EntryPerft entry) { //TODO: change replacement stratergy
+		table[entry.key % tableSize] = entry; //Adds new entry to table
 	}
-	public: pair<bool, uint64_t> get_count(uint64_t key, int depth) {
-		EntryPerft entry = table[key % tableSize];
-		if (entry.key == key && entry.depth == depth) {
-			//std::cout << key << ":" << entry.key << "\n";
-			//std::cout << "found" << "\n";
-			return make_pair(true, entry.count);
+	public: pair<bool, uint64_t> get_count(uint64_t key, int depth) { //Gets count from entry
+		EntryPerft entry = table[key % tableSize]; //Gets entry
+		if (entry.key == key && entry.depth == depth) { //If the entry key and depth match the one you are looking for
+			return make_pair(true, entry.count); //Return the count
 		}
-		return make_pair(false, 0);
+		return make_pair(false, 0); //Returns false to indicate it couldn't be found
 	}
 	public: void delete_table() {
-		delete[] table;
+		delete[] table; //Deletes table after use
 	}
 };
 
