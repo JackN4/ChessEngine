3fc1e17bba5249f17782c76a3835c8c92069bf7c
Jack <Jack@LAPTOP-4KELHCAM>
2021-11-11 13:12:28 +0000

    Fixed training mode

--- Chess/Search.h
+++ Chess/Search.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <random>
-#include <functional>
 #include "Board.h"
 #include "MoveCreator.h"
 #include "Evaluator.h"
@@ -16,10 +14,7 @@ class Search
 	int depthStart = 7; //The specified depth to search too
 	int max = 100000; //Used as infinity
 	Evaluator evaluator; 
-	std::default_random_engine generator;
-	std::uniform_real_distribution<double> distribution; //TODO: make more random
 	enum difficulty { easy, medium, hard , impossible}; //difficulties
-	double randomness[3] = { 0.1, 0.05, 0.01 }; //How much randomness is added
 	int depths[3] = { 3,4,5 }; //The depth searched to
 	difficulty diff = hard; //default difficulty
 
@@ -30,17 +25,7 @@ public: Move start_search(Board& board, int diffIn = 3) {
 		return negamax_iter(board).first;
 	}
 	else { //Limits search to a difficulty level
-		generator.seed(time(NULL)); //Check if works
-		SearchTable table; //Creates table
-		MoveCreator moveGen = MoveCreator(board); //Makes move generator
-		cout << "depth: " << depths[diff] << "\n";
-		cout << "difficulty: " << diff << "\n";
-		cout << "random: " << randomness[diff] << "\n";
-		negamax_diff(moveGen, depths[diff], -max, max, table); //Searches with specified
-		print_moves(board, table, 0);  
-		Move bestMove = moveGen.board.get_move_from_hash(table.get_entry(moveGen.board.zobristKey).second.bestMove); //Finds best move from hash table
-		table.delete_table(); //Deletes table
-		return bestMove;
+		return negamax_iter(board, depths[diff]).first;
 	}
 	
 }
@@ -90,14 +75,7 @@ public: void print_moves(Board& board, SearchTable& table, int depth) { //Prints
 	cout << "\n";
 }
 
-int random_score(int score) {
-	double multi = 2*randomness[diff]*(distribution(generator)-0.5);
-	int output = (score * (1 + multi));
-	//cout << "original score: " << score << "\n";
-	//cout << "output: " << (score * (1 + multi)) << "\n";
-	//cout << "mult: " << multi << "\n";
-	return (score * (1+multi));
-}
+
 
 private: int negamax(MoveCreator &moveGen, int depth, int alpha, int beta, SearchTable &table) { //Performs negamax search 
 	int origAlpha = alpha;
@@ -186,93 +164,6 @@ private: int negamax(MoveCreator &moveGen, int depth, int alpha, int beta, Searc
 	return alpha;
 }
 
-private: int negamax_diff(MoveCreator& moveGen, int depth, int alpha, int beta, SearchTable& table) { //Performs negamax search to specified difficulty
-	int origAlpha = alpha;
-	pair<bool, EntrySearch> entry = table.get_entry(moveGen.board.zobristKey); //Gets entry in transposition table from key
-	if (entry.first && entry.second.depth >= depth) {  //If entry depth is current depth or more
-			if (entry.second.node == 1) { //Node was fully searched
-				return entry.second.value;
-			}
-			else if (entry.second.node == 2) { //value is lower bound
-				if (entry.second.value > alpha) {
-					alpha = entry.second.value;
-				}
-			}
-			else if (entry.second.node == 3) { //value is upper bound
-				if (entry.second.value < beta) {
-					beta = entry.second.value;
-				}
-			}
-			if (alpha >= beta) {
-				return entry.second.value;
-			}
-		}
-	int score;
-	if (depth == 0) { //If depth is 0 an q search must be perform to make the board stable befor evaluation takes place
-		score = random_score(q_search(moveGen, alpha, beta, table)); //Adds randomness to the returned score
-		table.add(EntrySearch(moveGen.board.zobristKey, 0, score, 1)); //Results added to trans table
-		return score;
-	}
-	Move bestMove;
-	int bestScore = -(max + 1); //Best score starts at -inf so it can only be improved upon
-	if (entry.first) { //If entry in table exist
-		if (entry.second.bestMove.start != entry.second.bestMove.end) { //If it has a valid best move
-			Move move = moveGen.board.get_move_from_hash(entry.second.bestMove); //Gets move from entry 
-			moveGen.board.make_move(move); //Make move
-			score = random_score(-(negamax_diff(moveGen, depth - 1, -beta, -alpha, table))); //Adds randomness to the returned score
-			moveGen.board.unmake_move(move); //Unmakes move
-			if (score >= beta) { //Fail high
-				table.add(EntrySearch(moveGen.board.zobristKey, depth, score, 2, bestMove));
-				return beta;
-			}
-			if (score > bestScore) { //Sets bestscore and alpha
-				bestMove = move;
-				bestScore = score;
-				if (score > alpha) {
-					alpha = score;
-				}
-			}
-		}
-	}
-	vector<Move> allMoves = moveGen.get_all_moves(); //Generates all moves
-	if (allMoves.size() == 0) { //If there are no moves
-		if (moveGen.checkers != 0) {
-			; //If the king is in check
-			score = -max; //Checkmate
-		}
-		else { //If not it is stalemate
-			score = 0;
-		}
-		table.add(EntrySearch(moveGen.board.zobristKey, depth, score, 1)); //Adds result to transposition table
-		return score;
-	}
-
-	for (Move& move : allMoves) { //Iterates through moves
-		moveGen.board.make_move(move); //Makes move
-		score = random_score(-(negamax_diff(moveGen, depth - 1, -beta, -alpha, table))); //Adds randomness to the returned score
-		moveGen.board.unmake_move(move); //Unmakes move
-		if (score >= beta) { //Fail high
-			table.add(EntrySearch(moveGen.board.zobristKey, depth, score, 2, bestMove)); //Adds result to transposition table
-			return beta;
-		}
-		if (score > bestScore) { //Sets best score and alpha
-			bestMove = move;
-			bestScore = score;
-			if (score > alpha) {
-				alpha = score;
-			}
-		}
-	}
-	int nodeType;
-	if (bestScore <= origAlpha) { //Adds results to transposition table
-		nodeType = 3;
-	}
-	else {
-		nodeType = 1;
-	}
-	table.add(EntrySearch(moveGen.board.zobristKey, depth, bestScore, nodeType, bestMove));
-	return alpha; 
-}
 
 private: int q_search(MoveCreator& moveGen, int alpha, int beta, SearchTable& table) {//This search is done after the normal search to create a stable situation on the board by just searching captures
 	int currentEval;
